fd6d865ec3c305dbb79d641c4c59ea964a6f9483
Danielle Jenkins <d@d6e.io>
2016-05-29 12:16:41 -0700

    Clean up commenting and formatting.

--- trivial-shell.cpp
+++ trivial-shell.cpp
@@ -1,96 +1,88 @@
-#include <unistd.h>     
-#include <sys/types.h>  
-#include <sys/wait.h>   
-#include <signal.h>     
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <iostream>
 #include <vector>
 #include <stdio.h>
 #include <string.h>
 using namespace std;
 
-int main()
-{
-	vector<char*> args;
-	while (!cin.eof())
-    {
-		// get input
-		char cmd[128];
-		cin.getline(cmd, 128);
+int main() {
+    vector<char*> args;
+    while (!cin.eof()) {
+        // get input
+        char cmd[128];
+        cin.getline(cmd, 128);
 
-		char* cmdStr = strtok(cmd, " "); //collect token strings
-		
-	    while(cmdStr != NULL)
-	    {
-	    	char** argv = new char*[args.size() + 2]; //need an array of c-strings
+        char* cmdStr = strtok(cmd, " "); //collect token strings
 
-			for (int k = 0; k < args.size(); k++)
-			{
-				argv[k] = args[k];  // load command and arguments into the array of c-strings
-			}
-			
-			argv[args.size()] = NULL; //make sure the end is null
-			
-			if(strcmp(cmdStr, ";") == 0 && !args.empty()) // run a command
-		    {
-				pid_t childpid = fork(); // Fork you.
+        while(cmdStr != NULL) {
+            char** argv = new char*[args.size() + 2]; //need an array of c-strings
 
-				if (childpid < 0)
-				{
-					delete[] argv;
-					perror("Error: Fork failed.");
-					return 1;
-				}
-				else if (childpid == 0) // The child
-				{
-					cout << "executing \"" << argv[0] << "\"" << endl;
-					execv(argv[0], argv);
-					delete[] argv;
-					perror(argv[0]);
-					return 1;
-				}
-				else
-				{
-					int status = 0;
-					// This is the parent.  Need to wait for the child.
-					waitpid(childpid, &status, 0);
-					cout << "Child process exit status: " << status << endl;
-					for(int i = 0; i < args.size(); i++)
-						delete args[i]; 
-					args.clear();
-				}
-			}			
-			else if (strcmp(cmdStr, "exit") == 0) //exit if the command is "exit"
-			{
-				delete[] argv;
-				return 0;
-			}
-			else if (strcmp(cmdStr, "#") == 0 || strcmp(cmdStr, "#!") == 0) // comment
-			{
-				delete[] argv;
-				break;
-			}
-			else if (strcmp(cmdStr, "xyzzy") == 0)  //built-in "xyzzy" command
-		    {
-				cout << "Nothing happens." << endl;
-				delete[] argv;
-				break;
-			}
-			else
-			{
-				if (strcmp(cmdStr, ";") != 0) 
-				{
-					char *c = new char[128]; // have to create a new char string 
-					memcpy(c, cmdStr, 128); // otherwise the value of cmdStr gets re-written
-					args.push_back(c);       // while push_back takes an address (took far too long to figure out v.v)
-		        	cmdStr = strtok(NULL, " "); // then clear cmdStr so we can move on
-				}
-				else
-				{
-					cmdStr = strtok(NULL, " "); //clear cmdStr so we can move on
-				}
-			}
-			delete[] argv;
-	    }
+            for (int k = 0; k < args.size(); ++k) {
+                argv[k] = args[k];  // load command and arguments into the array of c-strings
+            }
+
+            argv[args.size()] = NULL; //make sure the end is null
+
+             // run a command
+            if(strcmp(cmdStr, ";") == 0 && !args.empty()) {
+                pid_t childpid = fork();
+
+                if (childpid < 0) {
+                    delete[] argv;
+                    perror("Error: Fork failed.");
+                    return 1;
+                }
+                // The child
+                else if (childpid == 0) {
+                    cout << "executing \"" << argv[0] << "\"" << endl;
+                    execv(argv[0], argv);
+                    delete[] argv;
+                    perror(argv[0]);
+                    return 1;
+                }
+                else {
+                    int status = 0;
+                    // This is the parent.  Need to wait for the child.
+                    waitpid(childpid, &status, 0);
+                    cout << "Child process exit status: " << status << endl;
+                    for(int i = 0; i < args.size(); ++i){
+                        delete args[i];
+                    }
+                    args.clear();
+                }
+            }
+            // exit if the command is 'exit'
+            else if (strcmp(cmdStr, "exit") == 0) {
+                delete[] argv;
+                return 0;
+            }
+            // Support commenting
+            else if (strcmp(cmdStr, "#") == 0 || strcmp(cmdStr, "#!") == 0) {
+                delete[] argv;
+                break;
+            }
+            //built-in "xyzzy" command
+            else if (strcmp(cmdStr, "xyzzy") == 0) {
+                cout << "Nothing happens." << endl;
+                delete[] argv;
+                break;
+            }
+            else {
+                if (strcmp(cmdStr, ";") != 0) {
+                    char *c = new char[128]; // have to create a new char string
+                    memcpy(c, cmdStr, 128); // otherwise the value of cmdStr gets re-written
+                    args.push_back(c);       // while push_back takes an address (took far too long to figure out v.v)
+                    cmdStr = strtok(NULL, " "); // then clear cmdStr so we can move on
+                }
+                else {
+                    cmdStr = strtok(NULL, " "); // clear cmdStr so we can move on
+                }
+            }
+            delete[] argv;
+        }
     }
-	return 0;
+    return 0;
 }
